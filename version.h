3f33fba5aaa33d6f0d15d60fbd678d3aa28c2d54
Patcharapol <patcharapol.loso@gmail.com>
2019-11-14 22:09:41 +0700

    Update version

--- version.h
+++ version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VER_FILEVERSION             2,3,0
-#define VER_FILEVERSION_STR         "2.3.0\0"
+#define VER_FILEVERSION             2,4,0
+#define VER_FILEVERSION_STR         "2.4.0\0"
 
 #define VER_PRODUCTVERSION          2,0,0
 #define VER_PRODUCTVERSION_STR      "2.0.0\0"
